0b47f9b8327917fbf6ddb1756deca508f57f2880
Gatotz <gatot.prastyo@an.tv>
2020-06-17 20:16:27 +0700

    Fix jumlah data dan pagination

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-	const allItems = 50;
+	const allItems = 100;
 	$('#data-tabel').each(function (append) {
 		let rowTr = '';
-		for (let i = 1; i < allItems; i++) {
+		for (let i = 0; i < allItems; i++) {
+			let rowNum = i + 1;
 			rowTr += '<tr>';
 			rowTr += '<td>' + '<input type="checkbox">' + '</td>';
-			rowTr += "<td>" + i + "</td>";
-			rowTr += "<td>" + "Nama" + "-" + i + "</td>";
-			rowTr += "<td>" + "Alamat" + "-" + i + "</td>";
+			rowTr += "<td>" + rowNum + "</td>";
+			rowTr += "<td>" + "Nama" + "-" + rowNum + "</td>";
+			rowTr += "<td>" + "Alamat" + "-" + rowNum + "</td>";
 			rowTr += "</tr>";
 		}
 		$('#data-tabel').append(rowTr);
@@ -15,46 +16,48 @@ $(document).ready(function(){
 
 	console.log("allItems", allItems);
 	let perPage = 10;
-	let totalPages = Math.ceil(allItems/perPage);
+	let totalPages = Math.ceil(allItems/perPage -1);
 	console.log('total halaman', totalPages);
-	  
+	
 	$("#data-tabel tbody tr").hide();
 	$("#data-tabel tbody tr").slice(0, perPage).show();
-	let currentPage = $(".pagination").index() - 1;
+	let currentPage = $(".pagination").index() -1;
 	console.log("halaman", currentPage);
- 
+	
 	$("#btnNext").click(function(){
-		currentPage ++;
-		console.log('halaman', currentPage);
-		let startPage = currentPage * perPage;
-		console.log('startPage', startPage);
-		let endPage = startPage + perPage;
-		console.log('endPage', endPage);
-		
-		for (i = 0;i < totalPages ; i++){
-			console.log('asasasasasa', i);
-			$("#data-tabel tbody tr").hide().slice(startPage, endPage).show();
-		};
+		if (currentPage === totalPages){
+			return false;
+		} else {
+			currentPage++;
+			let startPage = currentPage * perPage;
+			let endPage = startPage + perPage;
+			console.log("halaman", currentPage);
+			for (i = 0; i < totalPages; i++) {
+				// console.log('asasasasasa', i);
+				$("#data-tabel tbody tr").hide().slice(startPage, endPage).show();
+			};
+		}
 	})
 
-	$("#btnPrev").click(function(){
-		currentPage --;
-		console.log('halaman', currentPage);
-		let startPage = currentPage * perPage;
-		console.log('startPage', startPage);
-		let endPage = startPage + perPage;
-		console.log('endPage', endPage);
-		
-		for (i = 0;i < totalPages ; i++){
-			console.log('asasasasasa', i);
-			$("#data-tabel tbody tr").hide().slice(startPage, endPage).show();
-		};
+	$("#btnPrev").click(function () {
+		if (currentPage === 0) {
+			return false;
+		} else {
+			currentPage--;
+			let startPage = currentPage * perPage;
+			let endPage = startPage + perPage;
+			console.log("halaman", currentPage);
+			for (i = 0; i < totalPages; i++) {
+				// console.log('asasasasasa', i);
+				$("#data-tabel tbody tr").hide().slice(startPage, endPage).show();
+			};
+		}
 	})
 
 // Checked and unchecked list row
 	$('#data-tabel tr').click(function (event) {
 		let $target = $(event.target);
-		console.log('asdfsdkjb', $target);
+		// console.log('asdfsdkjb', $target);
 		if(!$target.is('input[type="checkbox"]'))
 		{
 			$(this).find('input[type="checkbox"]').each(function () {
@@ -65,15 +68,18 @@ $(document).ready(function(){
 		}
 		$(this).closest('tr').find('td').toggleClass('selectedRow');
 		rowSelected = $(this);
-		console.log('selected row', rowSelected);
+		// console.log('selected row', rowSelected);
 	});
 
 	$("#btnShowSelectedData").click(function(){
-		$("#table-selected-data").removeClass('.selectedRow');
 		let setRow = rowSelected.clone();
+		if(!setRow.is(true))
+		rowSelected.removeClass('selectedRow');
+		rowSelected.find("#selected-data").removeClass('td');
 		let getRow = $("#table-selected-data tbody");
 		getRow.append(setRow);
 		setRow++;
+		$(setRow).removeClass();
 		$("#table-selected-data td:first-child").hide();
 		// $("#table-selected-data td:first-child").remove();
 		// $("#table-selected-data").each(function(){
@@ -83,10 +89,4 @@ $(document).ready(function(){
 		// 	// }
 		// })
 	})
-			// $("#table-selected-data").each(function(){
-			// 	$(this).find("td:first-child").remove();
-			// 	// let boxSelected = $('input[type="checkbox"]');
-			// 	// console.log('box selected', boxSelected)
-			// 	// $(this).find(boxSelected).slice(1).remove();
-			// })
  })
